3741456658ef8623c15cf0f04a7a1bc0a1752bda
LordMuetze <valentin-herrmann@gmx.net>
2019-06-12 16:07:06 +0200

    CHG: changed mainWindow to alternativeWindow.ui DEL: wildcard import of classes DEL: content of  MainWindow.update() --> method template CHG: adjusted import dialog

--- mainView.py
+++ mainView.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets, uic
 from model import Vertretungsplan
-from classes import *
+#from classes import Stunde
 import sys
 
 class MainWindow:
@@ -9,19 +9,24 @@ class MainWindow:
 
         #create main window
         self.app = QtWidgets.QApplication([])
-        self.mW = uic.loadUi("mainWindow.ui")
+        #self.mW = uic.loadUi("mainWindow.ui")
+        self.mW = uic.loadUi("alternativeWindow.ui")
 
     
         #create model and set attribute
         #view calls model, not vice-versa! --> no self.view in model
         self.model = Vertretungsplan()
 
+        #self.mW.
+
+
         #Connections
         #self.mainWindow.{Ausloeser}.{Aktion}.connect({MethodenAufruf})
         #Bsp.: self.mW.pushButton.clicked.connect(self.test)
         self.mW.actionSpeichern.triggered.connect(self.on_actionSpeichern_triggered)
         self.mW.action_ffnen.triggered.connect(self.on_action_ffnen_triggered)
-        self.mW.actionEinlesen.triggered.connect(self.on_actionEinlesen_triggered)
+        #self.mW.actionEinlesen.triggered.connect(self.on_actionEinlesen_triggered)
+        self.mW.actionImportieren.triggered.connect(self.on_actionEinlesen_triggered)
 
         #launch GUI --> last step of __init__ because starts loop
         self.mW.show()
@@ -42,15 +47,11 @@ class MainWindow:
         
 
     def on_actionEinlesen_triggered(self):
-        pathUnter = QtWidgets.QFileDialog().getOpenFileName(None,"Datei Unter einlesen","","All Files (*)")[0]
-        pathZuordnung = QtWidgets.QFileDialog().getOpenFileName(None,"Datei Zuordnung einlesen","","All Files (*)")[0]
+        pathUnter = QtWidgets.QFileDialog().getOpenFileName(None,"Unter-Daei einlesen","","SPM Files (*.spm);;All Files (*)")[0]
+        pathZuordnung = QtWidgets.QFileDialog().getOpenFileName(None,"Zuordnungs-Datei einlesen","","SPM Files (*.spm);;All Files (*)")[0]
         self.model.DateienEinlesen(pathUnter,pathZuordnung)
         self.update()
 
     
     def update(self):
-        self.mW.ListeZuVertretendeLehrkraft.clear()
-        for i in Lehrer.LehrerListe():
-            self.mW.ListeZuVertretendeLehrkraft.addItem(str(i))
-
-        #self.mW.ListeZuVertretendeLehrkraft.addItems(Lehrer.LehrerListe())
+        pass
