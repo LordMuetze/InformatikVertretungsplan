dc240f6a79da226e4897d4e26e2194207a50c8e2
LordMuetze <valentin-herrmann@gmx.net>
2019-06-23 19:01:19 +0200

    ADD: update functionality for tw_problemStunde, tw_freieLehrer & tw_freieRaeume FIX: added one column to tw_vertretungsstunden to make col "Raum" visible ADD: clear() method that clear all TableWidgets by setting rowCount to 0, calling model.clearData() and setting self.warnungsliste to empty ADD: method that triggers change of selection in tw_problemStunden to auto display free teachers/rooms ADD: functionality for dialogs

--- mainView.py
+++ mainView.py
@@ -23,6 +23,8 @@ class MainWindow:
         #set datum to dateEdit
         self.datum = Tag.createTag(self.mW.de_mainDatum.date())
 
+        self.warnungsliste = []
+
 
         #Connections
         #QtCore.QMetaObject.connectSlotsByName(self.mW)
@@ -35,6 +37,7 @@ class MainWindow:
         self.mW.actionallgemeiner_Unterrichtsschluss.triggered.connect(self.on_actionallgemeiner_Unterrichtsschluss_eintragen_triggered)
         self.mW.btn_datumHeute.clicked.connect(self.on_btn_datumHeute_clicked)
         self.mW.de_mainDatum.dateChanged.connect(self.on_de_mainDatum_dateChanged)
+        self.mW.tw_problemStunden.itemSelectionChanged.connect(self.on_tw_problemStunden_selectionChanged)
 
 
         self.mW.show()
@@ -75,6 +78,7 @@ class MainWindow:
         self.mW.setEnabled(False)
         path = QtWidgets.QFileDialog().getOpenFileName(None,"Datei oeffnen","","CSV Files (*.csv);;All Files (*)")[0]
         if path != "":
+            self.clear()
             self.model.openCSV(path)
             self.update()
         self.mW.setEnabled(True)
@@ -87,6 +91,7 @@ class MainWindow:
         pathUnter = QtWidgets.QFileDialog().getOpenFileName(None,"Unter-Daei einlesen","","SPM Files (*.spm);;All Files (*)")[0]
         pathZuordnung = QtWidgets.QFileDialog().getOpenFileName(None,"Zuordnungs-Datei einlesen","","SPM Files (*.spm);;All Files (*)")[0]
         if pathUnter != "" and pathZuordnung != "":
+            self.clear()
             self.model.DateienEinlesen(pathUnter,pathZuordnung)
             self.update()
         self.mW.setEnabled(True)
@@ -94,28 +99,16 @@ class MainWindow:
 
 
     #--------------------------------------------------
-    def on_actionAbwesenheit_eintragen_triggered(self):
-        print("Works")
-    #--------------------------------------------------
-
-
-    #--------------------------------------------------
-    def on_actionRaum_blockieren_eintragen_triggered(self):
-        print("Works")
-    #--------------------------------------------------
-
-
-    #--------------------------------------------------
     def on_actionallgemeiner_Unterrichtsschluss_eintragen_triggered(self):
         self.dialog = uic.loadUi("DialogUnterrichtsschluss.ui")
-        self.dialog.btn_eintragen.clicked.connect(self.on_btn_eintragen_clicked)
+        self.dialog.btn_eintragen.clicked.connect(self.on_btn_unterrichtsschlussEintragen_clicked)
         self.dialog.calendarWidget.setSelectedDate(self.mW.de_mainDatum.date())
         self.dialog.show()
     #--------------------------------------------------
 
 
     #--------------------------------------------------
-    def on_btn_eintragen_clicked(self):
+    def on_btn_unterrichtsschlussEintragen_clicked(self):
         datum = self.dialog.calendarWidget.selectedDate()
         ab = self.dialog.sb_abStunde.value()
         tag = Tag.createTag(datum)
@@ -130,9 +123,92 @@ class MainWindow:
 
 
     #--------------------------------------------------
+    def on_actionRaum_blockieren_eintragen_triggered(self):
+        self.dialog = uic.loadUi("DialogBlockierung.ui")
+        self.dialog.btn_eintragen.clicked.connect(self.on_btn_blockierungEintragen_clicked)
+        self.dialog.calendarWidget.setSelectedDate(self.mW.de_mainDatum.date())
+
+        raumliste = Raum.RaumListe()
+        self.dialog.tw_alleRaeume.setRowCount(len(raumliste))
+        self.dialog.tw_alleRaeume.setColumnCount(1)
+        raumliste.sort()
+        for i,item in enumerate(raumliste):
+            self.dialog.tw_alleRaeume.setItem(i,0, QtWidgets.QTableWidgetItem(str(item)))
+
+        self.dialog.show()
+    #--------------------------------------------------
+
+
+    #--------------------------------------------------
+    def on_btn_blockierungEintragen_clicked(self):
+        datum = self.dialog.calendarWidget.selectedDate()
+        ab = self.dialog.sb_abStunde.value()
+        bis = self.dialog.sb_bisStunde.value()
+        ra = self.dialog.tw_alleRaeume.currentItem().text()
+        raum = list(filter(lambda c: str(c)==ra,Raum.RaumListe()))[0]
+        tag = Tag.createTag(datum)
+        Blockierung(raum,tag,ab,bis)
+        self.dialog.done(0)
+        self.update()
+    #--------------------------------------------------
+
+
+    #--------------------------------------------------
+    def on_actionAbwesenheit_eintragen_triggered(self):
+        self.dialog = uic.loadUi("DialogAbwesenheit.ui")
+        self.dialog.btn_eintragen.clicked.connect(self.on_btn_abwesenheitEintragen_clicked)
+        self.dialog.calendarWidget.setSelectedDate(self.mW.de_mainDatum.date())
+
+        lehrerliste = Lehrer.LehrerListe()
+        self.dialog.tw_alleLehrer.setRowCount(len(lehrerliste))
+        self.dialog.tw_alleLehrer.setColumnCount(1)
+        lehrerliste.sort()
+        for i,item in enumerate(lehrerliste):
+            self.dialog.tw_alleLehrer.setItem(i,0, QtWidgets.QTableWidgetItem(str(item)))
+
+        self.dialog.show()
+    #--------------------------------------------------
+
+
+    #--------------------------------------------------
+    def on_btn_abwesenheitEintragen_clicked(self):
+        datum = self.dialog.calendarWidget.selectedDate()
+        ab = self.dialog.sb_abStunde.value()
+        bis = self.dialog.sb_bisStunde.value()
+        leh = self.dialog.tw_alleLehrer.currentItem().text()
+        lehrer = list(filter(lambda c: str(c)==leh,Lehrer.LehrerListe()))[0]
+        tag = Tag.createTag(datum)
+        Blockierung(lehrer,tag,ab,bis)
+        self.dialog.done(0)
+        self.update()
+    #--------------------------------------------------
+
+
+    #--------------------------------------------------
     def on_de_mainDatum_dateChanged(self,date):
         self.datum = Tag.createTag(date)
         self.update()
+    #--------------------------------------------------
+
+
+    #--------------------------------------------------
+    def on_tw_problemStunden_selectionChanged(self):
+        self.update()
+    #--------------------------------------------------
+
+
+    #--------------------------------------------------
+    def clear(self):
+        self.model.clearData()
+        self.warnungsliste = []
+        self.mW.tw_alleLehrer.setRowCount(0)
+        self.mW.tw_alleRaeume.setRowCount(0)
+        self.mW.tw_blockierteRaeume.setRowCount(0)
+        self.mW.tw_abwesendeLehrer.setRowCount(0)
+        self.mW.tw_freieLehrer.setRowCount(0)
+        self.mW.tw_freieRaeume.setRowCount(0)
+        self.mW.tw_problemStunden.setRowCount(0)
+        self.mW.tw_vertretungsstunden.setRowCount(0)
     #--------------------------------------------------
 
 
@@ -194,7 +270,7 @@ class MainWindow:
         # update QtableWidget tw_vertretungsstunden
         vertretungsliste = self.datum.Ersatzstunden()
         self.mW.tw_vertretungsstunden.setRowCount(len(vertretungsliste))
-        self.mW.tw_vertretungsstunden.setColumnCount(4)
+        self.mW.tw_vertretungsstunden.setColumnCount(5)
         self.mW.tw_vertretungsstunden.setHorizontalHeaderLabels(["Klasse","Stunde","Fach","Vertretung","Raum"])
         vertretungsliste.sort()
         for i,item in enumerate(vertretungsliste):
@@ -208,6 +284,78 @@ class MainWindow:
             self.mW.tw_vertretungsstunden.setItem(i,2, QtWidgets.QTableWidgetItem(str(fach)))
             self.mW.tw_vertretungsstunden.setItem(i,3, QtWidgets.QTableWidgetItem(str(lehrer)))
             self.mW.tw_vertretungsstunden.setItem(i,4, QtWidgets.QTableWidgetItem(str(raum)))
+
+
+        # update QtableWidget tw_problemStunden
+        self.warnungsliste = []
+        stunden = self.datum.Stunden()
+        abw = self.datum.BlockierteLehrer()
+        block = self.datum.BlockierteRaeume()
+
+        for a in abw:
+            for s in stunden:
+                if a.BlockiertesObjekt() == s.Lehrer() and s.Stunde() > a.Von() and s.Stunde() < a.Bis():
+                    self.warnungsliste.append((s,"Lehrer"))
+        for b in block:
+            for s in stunden:
+                if b.BlockiertesObjekt() == s.Raum() and s.Stunde() > b.Von() and s.Stunde() < b.Bis():
+                    self.warnungsliste.append((s,"Raum"))
+
+        self.mW.tw_problemStunden.setRowCount(len(self.warnungsliste))
+        self.mW.tw_problemStunden.setColumnCount(6)
+        self.mW.tw_problemStunden.setHorizontalHeaderLabels(["Klasse","Stunde","Fach","Lehrer","Raum","Problem"])
+        self.warnungsliste.sort(key = lambda c: c[0])
+        for i,item in enumerate(self.warnungsliste):
+            s = item[0]
+            art = item[1]
+            klasse = s.Klasse()
+            stunde = s.Stunde()
+            lehrer = s.Lehrer()
+            raum = s.Raum()
+            fach = s.Fach()
+            self.mW.tw_problemStunden.setItem(i,0, QtWidgets.QTableWidgetItem(str(klasse)))
+            self.mW.tw_problemStunden.setItem(i,1, QtWidgets.QTableWidgetItem(str(stunde)))
+            self.mW.tw_problemStunden.setItem(i,2, QtWidgets.QTableWidgetItem(str(fach)))
+            self.mW.tw_problemStunden.setItem(i,3, QtWidgets.QTableWidgetItem(str(lehrer)))
+            self.mW.tw_problemStunden.setItem(i,4, QtWidgets.QTableWidgetItem(str(raum)))
+            self.mW.tw_problemStunden.setItem(i,5, QtWidgets.QTableWidgetItem(art))
+
+
+        # update TableWidgets for free Rooms and Teachers if problemStunde is selected
+        # if it's a room problem only show free rooms and vice versa
+        # clear Widgets first
+        self.mW.tw_freieLehrer.setRowCount(0)
+        self.mW.tw_freieRaeume.setRowCount(0)
+        if self.mW.tw_problemStunden.currentItem() is not None:
+            row = self.mW.tw_problemStunden.currentItem().row()
+            stunde = int(self.mW.tw_problemStunden.item(row,1).text())
+
+            if self.mW.tw_problemStunden.item(row,5).text() == "Lehrer":
+                # update tw_freieLehrer
+                lehrerliste = []
+                for lehrer in Lehrer.LehrerListe():
+                    l = list(filter(lambda c: c.Stunde() == stunde,lehrer.Stundenplan()[self.datum.dayOfWeek()-1]))
+                    if not l:
+                        lehrerliste.append(lehrer)
+
+                for i,item in enumerate(lehrerliste):
+                    self.mW.tw_freieLehrer.setRowCount(len(lehrerliste))
+                    self.mW.tw_freieLehrer.setColumnCount(1)
+                    self.mW.tw_freieLehrer.setItem(i,0, QtWidgets.QTableWidgetItem(str(item)))
+
+            if self.mW.tw_problemStunden.item(row,5).text() == "Raum":
+                # update tw_freieRaeume
+                raumliste = []
+                for raum in Raum.RaumListe():
+                    r = list(filter(lambda c: c.Stunde() == stunde,raum.Stundenplan()[self.datum.dayOfWeek()-1]))
+                    if not r:
+                        raumliste.append(raum)
+
+                for i,item in enumerate(raumliste):
+                    self.mW.tw_freieRaeume.setRowCount(len(raumliste))
+                    self.mW.tw_freieRaeume.setColumnCount(1)
+                    self.mW.tw_freieRaeume.setItem(i,0, QtWidgets.QTableWidgetItem(str(item)))
+
 
         self.mW.setEnabled(True)
     #--------------------------------------------------
